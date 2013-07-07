377036b13f634fe7faa903dbe91e8fac26f4ab2e
Tomasz Surowiec <tomasz.surowiec@gmail.com>
2013-07-07 14:48:09 +0000

    Change text origin

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var content = fs.readFile('index.html');
+    response.send(content);
 });
 
 var port = process.env.PORT || 5000;
