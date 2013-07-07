87d80d4d3edd039a12b024b89b09ee4c28fbdfe1
Tomasz Surowiec <tomasz.surowiec@gmail.com>
2013-07-07 14:56:39 +0000

    Using readFileSync

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var content = fs.readFile('index.html');
+    var content = fs.readFileSync('index.html');
     response.send(content);
 });
 
